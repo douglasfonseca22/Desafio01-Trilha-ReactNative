ae5bdbaee5075e6e9ee3d43d148b023b328de19e
Douglas Fonseca <douglasfonseca2010@hotmail.com>
2021-08-26 23:20:09 -0400

    main

---
+++ src/components/taskItem.tsx
@@ -0,0 +1,169 @@
+import React, { useRef, useState, useEffect } from 'react';
+import {
+    Image,
+    TouchableOpacity,
+    View,
+    Text,
+    StyleSheet,
+    TextInput
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { EditTaskArgs } from '../pages/Home';
+import { Task } from './TasksList';
+import trashIcon from '../assets/icons/trash/trash.png'
+import editIcon from '../assets/icons/edit/edit.png'
+// import { Container } from './styles';
+
+interface TaskItemProps {
+    task: Task;
+    toggleTaskDone: (id: number) => void;
+    removeTask: (id: number) => void;
+    editTask: ({ taskId, taskNewTitle }: EditTaskArgs) => void;
+}
+
+export function TaskItem({ task, editTask, removeTask, toggleTaskDone }: TaskItemProps) {
+    const [isEditing, setIsEditing] = useState(false)
+    const [taskNewTitleValue, setTaskNewTitleValue] = useState(task.title)
+    const textInputRef = useRef<TextInput>(null)
+
+    function handleStarEditing() {
+        setIsEditing(true);
+    }
+
+    function handleCancelEditing() {
+        setTaskNewTitleValue(task.title);
+        setIsEditing(false);
+    };
+
+    function handleSubmitEditing() {
+        editTask({ taskId: task.id, taskNewTitle: taskNewTitleValue });
+        setIsEditing(false)
+    }
+
+    useEffect(() => {
+        if (textInputRef.current) {
+            if (isEditing) {
+                textInputRef.current.focus();
+            } else {
+                textInputRef.current.blur();
+            }
+        }
+    }, [isEditing])
+    return (
+        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+            <View>
+                <TouchableOpacity
+                    activeOpacity={0.7}
+                    style={styles.taskButton}
+                    onPress={() => toggleTaskDone(task.id)}
+                >
+                    <View
+                        style={task.done ? styles.taskMarkerDone : styles.taskMarker}
+                    >
+                        {task.done && (
+                            <Icon
+                                name="check"
+                                size={12}
+                                color="#FFF"
+                            />
+                        )}
+                    </View>
+                    <TextInput
+                        value={taskNewTitleValue}
+                        onChangeText={setTaskNewTitleValue}
+                        editable={isEditing}
+                        onSubmitEditing={handleSubmitEditing}
+                        style={task.done ? styles.taskTextDone : styles.taskText}
+                        ref={textInputRef}
+                    />
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.iconsContainer}>
+                {isEditing ? (
+                    <TouchableOpacity
+                        onPress={handleCancelEditing}
+                    >
+                        <Icon name="x" size={24} color="#b2b2b2" />
+                    </TouchableOpacity>
+                ) : (
+                    <TouchableOpacity
+                        onPress={handleStarEditing}
+                    >
+                        <Image source={editIcon} />
+                    </TouchableOpacity>
+                )}
+                <View  style={styles.iconsDivider} />
+
+                <TouchableOpacity
+                    onPress={() => removeTask(task.id)}
+                    disabled={isEditing}
+                >
+                    <Image source={trashIcon} style={{ opacity: isEditing ? 0.2 : 1 }} />
+                </TouchableOpacity>
+            </View>
+        </View >
+    );
+}
+
+const styles = StyleSheet.create({
+    constainer: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    infoContainer: {
+        flex: 1
+    },
+    taskButton: {
+        flex: 1,
+        paddingHorizontal: 24,
+        marginBottom: 4,
+        borderRadius: 4,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    taskMarker: {
+        height: 16,
+        width: 16,
+        borderRadius: 4,
+        borderWidth: 1,
+        borderColor: '#B2B2B2',
+        marginRight: 15,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    taskText: {
+        color: '#666',
+        fontFamily: 'Inter-Medium'
+    },
+    taskMarkerDone: {
+        height: 16,
+        width: 16,
+        borderRadius: 4,
+        backgroundColor: '#1DB863',
+        marginRight: 15,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    taskTextDone: {
+        color: '#1DB863',
+        textDecorationLine: 'line-through',
+        fontFamily: 'Inter-Medium'
+    },
+    iconsContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingLeft: 12,
+        paddingRight: 24
+    },
+    iconsDivider: {
+        width: 1,
+        height: 24,
+        backgroundColor: 'rgba(196, 196, 196, 0.24)',
+        marginHorizontal: 12
+    }
+})
+
+export default TaskItem;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Alert } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+export type EditTaskArgs = {
+  taskId: number;
+  taskNewTitle: string;
+}
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+
+    const taskWithSameTitle = tasks.find(task => task.title === newTaskTitle);
+
+    if (taskWithSameTitle) {
+      return Alert.alert('Task já cadastrada', 'Você não pode cadastrar uma task com o mesmo nome')
+    }
     const newTask = {
       id: new Date().getTime(),
       title: newTaskTitle,
@@ -19,33 +30,57 @@ export function Home() {
   }
 
   function handleToggleTaskDone(id: number) {
-    const updateTask = tasks.map(tasks => ({ ...tasks}))
+    const updateTask = tasks.map(tasks => ({ ...tasks }))
 
     const foundItem = updateTask.find(item => item.id === id);
 
     if (!foundItem)
-    return;
+      return;
 
     foundItem.done = !foundItem.done;
     setTasks(updateTask);
   }
 
   function handleRemoveTask(id: number) {
-    const updateTask = tasks.filter(task => task.id !== id);
+    Alert.alert('Remover item', 'Tem certeza que deseja remover esse item?', [
+      {
+        style: 'cancel',
+        text: 'Não'
 
-    setTasks(updateTask);
+      },
+      {
+        style: 'destructive',
+        text: 'Sim',
+        onPress: () => {
+          const updateTask = tasks.filter(task => task.id !== id);
+          setTasks(updateTask);
+        }
+      }
+    ])
   }
 
+  function handleEditTask({ taskId, taskNewTitle }: EditTaskArgs) {
+    const updateTask = tasks.map(task => ({ ...task }))
+
+    const taskTobeUpdate = updateTask.find(task => task.id === taskId);
+
+    if (!taskTobeUpdate)
+      return;
+
+    taskTobeUpdate.title = taskNewTitle;
+    setTasks(updateTask);
+  }
   return (
     <View style={styles.container}>
       <Header tasksCounter={tasks.length} />
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
+        editTask={handleEditTask}
       />
     </View>
   )
